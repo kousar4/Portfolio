8486b944160af47ae0e0dda7e80564cd1f26ffb6
kousarnaaz <kousarnaazz@gmail.com>
2024-01-23 15:58:55 -0500

    backend code

---
+++ backend_sanity/schemas/index.ts
@@ -0,0 +1,19 @@
+import testimonials from './testimonials'
+import abouts from './abouts'
+import works from './works'
+import brands from './brands'
+import experiences from './experiences'
+import skills from './skills'
+import workExperience from './workExperience'
+import contact from './contact'
+
+export const schemaTypes = [
+    testimonials,
+    abouts,
+    brands,
+    contact,
+    experiences,
+    skills,
+    workExperience,
+    works
+]
