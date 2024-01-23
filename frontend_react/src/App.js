12a6caa49c46e6840d10b3c625dc9c2ed59e3ea1
kousarnaaz <kousarnaazz@gmail.com>
2024-01-23 16:03:41 -0500

    frontend code

---
+++ frontend_react/src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+import { About, Footer, Header, Skills, Testimonial, Work } from './containers'
+import { Navbar } from './components';
+import './App.scss';
+
+const App = () => (
+  <div className="app">
+    <Navbar />
+    <Header />
+    <About />
+    <Work />
+    <Skills />
+    <Testimonial />
+    <Footer />
+  </div>
+);
+
+export default App;
